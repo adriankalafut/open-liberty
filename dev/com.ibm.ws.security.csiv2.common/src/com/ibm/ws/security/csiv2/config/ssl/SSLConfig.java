e9a3366dddad9d1f140918058ac4d31791c02a1f
Karel-Harjono <Karel.Harjono@ibm.com>
2023-12-11 16:22:45 -0500

    enable hostname verification by default

--- dev/com.ibm.ws.security.csiv2.common/src/com/ibm/ws/security/csiv2/config/ssl/SSLConfig.java
+++ dev/com.ibm.ws.security.csiv2.common/src/com/ibm/ws/security/csiv2/config/ssl/SSLConfig.java
@@ -350,7 +350,7 @@ public class SSLConfig {
             return false;
         }
 
-        return Boolean.valueOf(sslProps.getProperty(Constants.SSLPROP_HOSTNAME_VERIFICATION, "false"));
+        return Boolean.valueOf(sslProps.getProperty(Constants.SSLPROP_HOSTNAME_VERIFICATION, "true"));
     }
 
     /**
